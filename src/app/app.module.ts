336f8d3ce0c60f5cab29cebbf793e29a03584efe
MoussaDev <44073284+moussa9594@users.noreply.github.com>
2022-12-07 22:37:24 +0000

    HR portal updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import { BrowserModule, HammerModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { IgxIconModule, IgxAvatarModule, IgxButtonModule, IgxRippleModule, IgxListModule, IgxInputGroupModule, IgxSelectModule, IgxCardModule, IgxNavbarModule } from 'igniteui-angular';
-import { FormsModule } from '@angular/forms';
 import { MyInfoComponent } from './my-info/my-info.component';
+import { IgxButtonModule, IgxRippleModule, IgxIconModule, IgxAvatarModule, IgxInputGroupModule, IgxSelectModule, IgxListModule, IgxCardModule, IgxNavbarModule } from 'igniteui-angular';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     MyInfoComponent,
+    HomeComponent,
     TestComponent
   ],
   imports: [
@@ -21,14 +21,14 @@ import { TestComponent } from './test/test.component';
     HammerModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    IgxIconModule,
-    IgxAvatarModule,
     IgxButtonModule,
     IgxRippleModule,
-    IgxListModule,
-    FormsModule,
+    IgxIconModule,
+    IgxAvatarModule,
     IgxInputGroupModule,
     IgxSelectModule,
+    FormsModule,
+    IgxListModule,
     IgxCardModule,
     IgxNavbarModule
   ],
